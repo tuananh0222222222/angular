72f77c8ed305a188903695125a1f09e8a5861099
kta-wd <xaydagolds@gmail.com>
2023-09-12 22:46:29 +0700

    #1- add day 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  public name: string = 'TUan anh dsdsds' ;
+  public age: number = 19;
+  
+  public resetName() : void {
+   this.name = '';
+  }
 
+  public traicay: string[]  = ["Tao","Nho","Xoai"]
+
+  public sothutu: number[] = [1,2,3,4,5,6,7,8]
+
+  public people: {names: string,ages:number} [] =[
+    {names:"john",ages:15},
+    {names:"john 1",ages:153},
+    {names:"john 2",ages:154} ,
+    {names:"john 3",ages:154}
+  ]
+
+ 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(this.traicay);
+  }
 }
